83327594da4079691f8507f4e4f133ced3b9fb20
Tyler Peterson <petersontb@familysearch.org>
2014-03-10 20:26:16 -0600

    Reuse ecstatic for serving static files.
    Hack ecstatic to serve static files since they’ve already figured out
    setting mime-types and whatnot.
    Hack the serve function to only serve files on the tag list.

--- app.js
+++ app.js
@@ -2,10 +2,18 @@
 // ex.: node app.js Work
 
 var http = require('http'),
-    fs = require('fs'),
     exec = require('child_process').exec,
     argv = require('minimist')(process.argv.slice(2)),
-    tagname = argv._[0];
+    tagname = argv._[0],
+    ecstatic = require('ecstatic'),
+    hackedServe;
+
+(function () {
+  var ecstaticMiddleware = ecstatic({root: '/'});
+  hackedServe = function (localFilename, res) {
+    ecstaticMiddleware({url: localFilename, method: 'GET'}, res);
+  };
+})();
 
 exec('tag -f ' + tagname, function (err, stdout, stderr) {
   if (err) {
@@ -16,16 +24,16 @@ exec('tag -f ' + tagname, function (err, stdout, stderr) {
   files.pop(); // Split returns an empty last entry. Discard it.
   console.log('files for tag:', tagname, files);
   http.createServer(function (req, res) {
+    console.log('request', req.url);
+    if (req.url === '/favicon.ico') {
+      console.log('failing request for favicon.')
+      res.writeHead(404);
+      res.end();
+      return;
+    }
     var file = files.shift();
     console.log('serving file:', file);
-    fs.readFile(file, function (err, data) {
-      if (err) {
-        console.log('err reading file', err);
-        return;
-      }
-      res.writeHead(200);
-      res.end(data);
-    });
+    hackedServe(file, res);
   }).listen(5000);
 });
 
